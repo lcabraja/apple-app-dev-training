f198b3fa68553188cf95304f87a8c9ed7c8bc36f
Luka Cabraja <luka@cabraja.eu>
2022-08-11 18:56:10 +0200

    initialized first project

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by doss on 8/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

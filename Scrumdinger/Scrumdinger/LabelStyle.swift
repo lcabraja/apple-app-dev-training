64af98c75b780f2f687617b211dc008a33875006
Luka Cabraja <luka@cabraja.eu>
2022-08-11 19:45:52 +0200

    completed lesson 2

---
+++ Scrumdinger/Scrumdinger/LabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  LabelStyle.swift
+//  Scrumdinger
+//
+//  Created by lcabraja on 8/11/22.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
